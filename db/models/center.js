0116501f241df029bd73cd9112f2e469b6a39b86
Mark Warren Lalata <mw.lalata@jumpdigital.asia>
2018-07-12 15:33:01 +0800

    validation for centers table

--- db/models/center.js
+++ db/models/center.js
@@ -1,12 +1,34 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Center = sequelize.define('Center', {
-    name: DataTypes.STRING,
-    long: DataTypes.INTEGER,
-    lat: DataTypes.INTEGER
+    name: {
+    	type: DataTypes.STRING,
+    	allowNull: false,
+    	validate: {
+    		min: 2,
+    		notEmpty: true
+    	}
+    },
+    long: {
+    	type: DataTypes.INTEGER,
+    	allowNull: false,
+    	validate: {
+    		isFloat: true,
+    		notEmpty: true
+    	}
+    },
+    lat: {
+    	type: DataTypes.INTEGER,
+    	allowNull: false,
+    	validate: {
+    		isFloat: true,
+    		notEmpty: true
+    	}
+    }
   }, {
   	underscored: true
   });
+  
   Center.associate = function(models) {
     // associations can be defined here
   };
