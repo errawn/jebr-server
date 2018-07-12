0e3da24e6ba43bf7ce518c7b31cb14385f0267b2
Mark Warren Lalata <mw.lalata@jumpdigital.asia>
2018-07-13 03:33:27 +0800

    enable cors and use morgan logger

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { graphiqlExpress, graphqlExpress } from 'apollo-server-express';
-import { makeExecutableSchema } from 'graphql-tools';
-import cors from "cors";
-
-import typeDefs from './schema';
-import resolvers from './resolvers';
-
-import models from './db/models';
+import express from 'express'
+import bodyParser from 'body-parser'
+import morgan from 'morgan'
+import cors from 'cors'
+// Graphql
+import { graphiqlExpress, graphqlExpress } from 'apollo-server-express'
+import { makeExecutableSchema } from 'graphql-tools'
+import typeDefs from './schema'
+import resolvers from './resolvers'
+// Db
+import models from './db/models'
 
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
-});
+})
 
-const app = express();
-
-app.use(cors());
-
-app.use(
-  '/graphiql',
-  graphiqlExpress({
-    endpointURL: '/graphql',
-  }),
-);
-
+const app = express()
+app.use(cors('*'))
+app.use(morgan('dev'))
 app.use(
 	'/graphql', 
 	bodyParser.json(), 
 	graphqlExpress({ schema, context: { models } })
-);
+)
+// Use graphiql
+app.use(
+  '/graphiql',
+  graphiqlExpress({
+    endpointURL: '/graphql',
+  })
+)
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000
 app.listen(PORT, () => console.log('App running!'));
