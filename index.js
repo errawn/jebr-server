e53913cf9c5285f86c0f06f8650f4528d12e67be
Mark Warren Lalata <mw.lalata@jumpdigital.asia>
2018-07-12 13:25:18 +0800

    Initial Schema

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { graphiqlExpress, graphqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
+
+import typeDefs from './schema';
+import resolvers from './resolvers';
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
 
 const app = express();
 
@@ -11,7 +20,7 @@ app.use(
   }),
 );
 
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphQLSchema }));
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema, }));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
