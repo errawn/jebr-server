c17bdd7f77155292345cf3e2ba271c001da5c70a
Mark Warren Lalata <mw.lalata@jumpdigital.asia>
2018-07-16 13:33:13 +0800

    createCenter mutation

--- resolvers/center.js
+++ resolvers/center.js
@@ -1,5 +1,15 @@
 export default {
 	Query: {
 		centers: (parent, args, { models }) => models.Center.findAll()
-	}
+	},
+	Mutation: {
+		createCenter: async (_, args, { models }) => {
+			try {
+				const center = await models.Center.create(args) 
+				return center
+			} catch(err) {
+				console.log(err)
+			}
+		}
+	},
 }
